1f5cce4d509fdc2fc5da8a210080cfbdd137ccfc
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-11 14:28:17 -0700

    pushed keys to heroku, adding geolocation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/departures/', to: 'application#departures'
 
+  root to: 'application#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
